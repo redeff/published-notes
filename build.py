5e2d9232ed1aada5c0ad1a309da105088000c7fc
Carlos Miguel Soto <14carlosoto@gmail.com>
2019-10-16 11:27:59 -0300

    retoques

--- build.py
+++ build.py
@@ -20,7 +20,7 @@ def with_path(path):
     if os.path.normpath(dr) == '.':
         return template.replace('{prefix}', '.')
     else:
-        return template.replace('{prefix}', '../../../')
+        return template.replace('{prefix}', '../../..')
 
 def comp(d):
     raw = os.path.splitext(d)[0]
@@ -59,13 +59,47 @@ for d in glob.glob('uba/*/*/*.tex'):
     files.append(comp(d))
 
 item = read_from_file('note_item.html')
+clase = read_from_file('class.html')
 
 write_to_file('index.html', with_path('index.html').replace('{content}',
     read_from_file('index_pre.html')))
 
-body = ""
+mappings = {
+    "algebra-i": "Álgebra I",
+    "analisis-i": "Análisis I",
+    "haskell": "Taller de Álgebra I",
+    "lineal": "Álgebra Lineal",
+}
+
+tipo = {
+    "teor" : "Teórica",
+    "prac" : "Práctica"
+}
+
+found = dict()
+
 for f in files:
-    body += item.replace('{link}', f).replace('{content}', f)
+    parts = f.split("/")
+    if not parts[1] in found:
+        found[parts[1]] = []
+    found[parts[1]].append(f)
+    # body += item.replace('{link}', f).replace('{content}', f)
+
+body = ""
+for key in found:
+    this_body = ""
+    for link in found[key]:
+        name = link.split("/")[2].split("-", 1)
+        tip = ""
+        if name[0] in tipo:
+            tip = tipo[name[0]] + " "
+        tip += name[len(name)-1]
+        this_body += item \
+            .replace('{link}', link) \
+            .replace('{content}', tip);
+    body += clase \
+        .replace('{content}', mappings[key]) \
+        .replace('{links}', this_body)
 
 write_to_file('notes.html', with_path('notes.html').replace('{content}',
     read_from_file('notes_pre.html').replace('{content}', body)))
